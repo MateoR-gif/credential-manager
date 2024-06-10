3503d135a26dc581247dec1407477a365ff82602
SanSpeen <ktns0930@gmail.com>
2024-06-10 18:08:09 -0500

    register and login page added at the app starting

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,26 +1,27 @@
 import 'package:app/providers/articles_provider.dart';
 import 'package:app/screens/HomeScreen.dart';
+import 'package:app/screens/LoginScreen.dart';
+import 'package:app/screens/RegisterPage.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(
     ChangeNotifierProvider(
       create: (context) => ArticlesProvider()..fetchArticles(),
-      child: MyApp(),
+      child: MaterialApp(
+        title: 'Your App Name',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        initialRoute: '/login', // Primero lleva al login
+        routes: {
+          '/login': (context) => LoginPage(),
+          '/register': (context) => RegisterPage(),
+          '/home': (context) => HomeScreen(),
+        },
+      ),
     ),
   );
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        dividerColor:
-            Colors.transparent, // Eliminar las líneas entre ExpansionTiles
-      ),
-      home: HomeScreen(),
-    );
-  }
 }
