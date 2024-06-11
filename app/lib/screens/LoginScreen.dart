97cea7e286215a6aa992248d073d540bb446108d
SanSpeen <ktns0930@gmail.com>
2024-06-10 20:13:46 -0500

    Login funcionando

--- app/lib/screens/LoginScreen.dart
+++ app/lib/screens/LoginScreen.dart
@@ -1,6 +1,14 @@
+import 'package:app/controllers/UserController.dart';
+import 'package:app/services/FirebaseAuthSevice.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final FireBaseAuthService _auth = FireBaseAuthService();
+  final UserController _userController = UserController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,17 +21,19 @@ class LoginPage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             TextFormField(
+              controller: emailController,
               decoration: InputDecoration(labelText: 'Correo electrónico'),
             ),
             SizedBox(height: 20.0),
             TextFormField(
+              controller: passwordController,
               obscureText: true,
               decoration: InputDecoration(labelText: 'Contraseña'),
             ),
             SizedBox(height: 20.0),
             ElevatedButton(
               onPressed: () {
-                Navigator.pushReplacementNamed(context, '/home');
+                signIn(context);
               },
               child: Text('Iniciar Sesión'),
             ),
@@ -39,4 +49,31 @@ class LoginPage extends StatelessWidget {
       ),
     );
   }
-} 
+
+  void signIn(BuildContext context) async {
+    final String email = emailController.text;
+    final String password = passwordController.text;
+
+    User? firebaseUser = await _auth.signInWithEmailAndPassword(email, password);
+    if (email.isNotEmpty && password.isNotEmpty && firebaseUser != null) {
+      Navigator.pushReplacementNamed(context, '/home');
+    } else {
+      // Mostrar un diálogo de error si los campos están vacíos
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Error'),
+          content: Text('Por favor, ingresa tu correo electrónico y contraseña.'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Aceptar'),
+            ),
+          ],
+        ),
+      );
+    }
+  }
+}
